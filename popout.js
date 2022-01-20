d8ad2cbc1e387ed229b60a4679fe892e1013b0e0
James McArthur <jamesmca@live.com>
2022-01-20 09:53:29 -0800

    finished

--- popout.js
+++ popout.js
@@ -6,7 +6,7 @@ document.querySelector('#create-todo').addEventListener('click', function () {
         let itemUrl = document.querySelector('#new-item input').value
         if (itemName) {
             let items = localStorage.getItem('learn-items')
-            let itemsArr = JSON.parse(items) !== null ? JSON.parse(items) : []
+            let itemsArr = JSON.parse(items) != null ? JSON.parse(items) : []
             itemsArr.push({ item: itemName, url: itemUrl })
             saveItems(itemsArr)
             fetchItems()
@@ -22,9 +22,8 @@ document.querySelector('#add-current-tab').addEventListener('click', function ()
         let itemName = tab[0].title
         let itemUrl = tab[0].url
         if (itemName) {
-            console.log(itemUrl)
             let items = localStorage.getItem('learn-items')
-            let itemsArr = JSON.parse(items) !== null ? JSON.parse(items) : []
+            let itemsArr = JSON.parse(items) != null ? JSON.parse(items) : []
             itemsArr.push({ item: itemName, url: itemUrl })
             saveItems(itemsArr)
             fetchItems()
@@ -32,19 +31,17 @@ document.querySelector('#add-current-tab').addEventListener('click', function ()
     })
 })
 
-document.querySelectorAll('.arrow-up').forEach(val => val.addEventListener('click', function () {
 
-}))
 
 
 function saveItems(obj) {
-    let string = JSON.stringify(obj)
+    let string = JSON.stringify(obj.filter(val => val != null))
     localStorage.setItem('learn-items', string)
 }
 
 function itemDelete(index) {
     let items = localStorage.getItem('learn-items')
-    let itemsArr = JSON.parse(items) !== null ? JSON.parse(items) : []
+    let itemsArr = JSON.parse(items) != null ? JSON.parse(items) : []
     document.querySelector('ul li[data-itemIndex="' + index + '"]').remove()
     saveItems(itemsArr.filter((val, i) => i !== index))
     fetchItems()
@@ -52,16 +49,21 @@ function itemDelete(index) {
 
 function moveUp(index) {
     let items = localStorage.getItem('learn-items')
-    let itemsArr = JSON.parse(items) !== null ? JSON.parse(items) : []
-    document.querySelector('ul li[data-itemIndex="' + index + '"]').remove()
-    saveItems(itemsArr.filter((val, i) => i !== index))
+    let itemsArr = JSON.parse(items) != null ? JSON.parse(items) : []
+    let temp = itemsArr[index - 1]
+    itemsArr[index - 1] = itemsArr[index]
+    itemsArr[index] = temp
+    saveItems(itemsArr)
     fetchItems()
 }
 
 function moveDown(index) {
     let items = localStorage.getItem('learn-items')
-    let itemsArr = JSON.parse(items) !== null ? JSON.parse(items) : []
-    saveItems(itemsArr.filter((val, i) => i !== index))
+    let itemsArr = JSON.parse(items) != null ? JSON.parse(items) : []
+    let temp = itemsArr[index + 1]
+    itemsArr[index + 1] = itemsArr[index]
+    itemsArr[index] = temp
+    saveItems(itemsArr)
     fetchItems()
 }
 
@@ -72,24 +74,24 @@ function fetchItems() {
     let newItemHTML = ''
     try {
         let items = localStorage.getItem('learn-items')
-        console.log(items)
-        let itemsArr = JSON.parse(items) !== null ? JSON.parse(items) : []
+        let itemsArr = JSON.parse(items) != null ? JSON.parse(items) : []
         for (let i = 0; i < itemsArr.length; i++) {
             const url = !!itemsArr[i].url ? itemsArr[i].url : '#'
+
             newItemHTML += `<li data-itemIndex="${i}" 
             class="flex justify-between items-center capitalize ">
                 <div class="flex-1 max-h-10 truncate">
-                    <a href="${url}" target="_blank">
-                        ${i + 1}: ${itemsArr[i].item} 
-                    </a>
+                    ${url !== "#" ? `<a href="${url}" target="_blank">
+                        ${i + 1}: ${itemsArr[i].item}
+                    </a>` : `${i + 1}: ${itemsArr[i].item}`}
                 </div>
                 <div class="flex items-center">
                     <button class="text-4xl">␡</button>
                     <div class="text-sm">
                         ${i > 0 ? '<div class="cursor-pointer arrow-up">&uarr;</div>' : ''}
-                        <div class="cursor-pointer arrow-down">&darr;</div>
+                        ${i < itemsArr.length - 1 ? '<div class="cursor-pointer arrow-down">&darr;</div>' : ''}
                     </div>
-                </div>
+                </div> 
             </li>`
         }
         itemsList.innerHTML = newItemHTML
@@ -99,6 +101,16 @@ function fetchItems() {
                 itemDelete(i)
             })
         })
+
+        document.querySelectorAll('.arrow-up').forEach((val, i) => val.addEventListener('click', function () {
+            moveUp(i + 1)
+
+        }))
+
+        document.querySelectorAll('.arrow-down').forEach((val, i) => val.addEventListener('click', function () {
+            moveDown(i)
+
+        }))
     } catch (err) {
         console.log(err)
         //create default items list
